a442b66c0468cc4edc4619fb0e61419494d6ea6c
Cesar Salinas <cesarsv93@gmail.com>
2018-07-27 16:31:29 -0500

    Trasferring files from my windows machine  to debian

---
+++ index.js
@@ -0,0 +1,10 @@
+//Import library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+
+//Create a component
+const App = () => <Header headerText={'Albums'} />;
+
+//Render it to device
+AppRegistry.registerComponent('albums', () => App);
